c85ffa0b1b1726a0eee32cbaa2dd682dfb9d29ed
HP <HP@DESKTOP-3MB2CKA>
2025-08-12 21:17:49 +0530

    Dev1 commit and pushed

---
+++ GitProject01/src/com/git/payment/UPIPayment.java
@@ -0,0 +1,9 @@
+package com.git.payment;
+
+public class UPIPayment
+{
+   public String doPaymentUsingPhonePe(long number,double amount)
+   {
+	   return amount+"amount payment has been done through"+number+"using Phonepay ";
+   }
+}
