9bd9fb4b0ff117fdb5357e2dae43cf8bcbf3a221
HP@DESKTOP-3MB2CKA <HP@DESKTOP-3MB2CKA>
2025-08-13 10:53:25 +0530

    TL added new method commit and pushed B1

---
+++ GitProject01/src/com/git/UPILitePayment/UPILitePayment.java
@@ -0,0 +1,9 @@
+package com.git.UPILitePayment;
+
+public class UPILitePayment
+{
+public String doPaymentUsingPhonePe(long mobileno,double amount)
+	{
+		return amount+"Amount payment done through PhonePe";
+	}
+}
